89c0dcde4992334b3dea35abe00af0cbaad3a688
Liam Craver <studios.lime@gmail.com>
2012-12-30 22:57:44 -0600

    setting up basic screen manager and splash screen

---
+++ TM2/GameScreen.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace TM2
+{
+    public class GameScreen
+    {
+        public virtual void Initialize() { }
+        public virtual void LoadContent(ContentManager Content) { }
+        public virtual void Update(GameTime gametime) { }
+        public virtual void Draw(SpriteBatch spriteBatch) { }
+    }
+}
---
+++ TM2/ScreenManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace TM2
+{
+    public class ScreenManager
+    {
+
+        #region Variables
+
+        /// <summary>
+        /// Screen Manager Instance ( this is good coding right ;) )
+        /// </summary>
+
+        private static ScreenManager instance;
+
+        /// <summary>
+        /// Screen Stacking 
+        /// </summary>
+
+        Stack<GameScreen> screenStack = new Stack<GameScreen>();
+
+        /// <summary>
+        /// Screen's width and height
+        /// </summary>
+
+        Vector2 dimensions;
+
+        #endregion
+
+        #region Properties
+
+        public static ScreenManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new ScreenManager();
+                return instance;
+            }
+        }
+
+        //you can do this directly in the vector but this makes it easier to visualize
+        public Vector2 Dimensions
+        {
+            get { return dimensions; }
+            set { dimensions = value; }
+        }
+
+        #endregion
+    }
+}
