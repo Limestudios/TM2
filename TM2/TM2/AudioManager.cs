f3d1f8bc4d9aaee127d5b8dc070577ad5c01672c
Pete <pete800@optonline.net>
2013-01-10 18:02:00 -0500

    Did some work on Audio.

--- TM2/TM2/AudioManager.cs
+++ TM2/TM2/AudioManager.cs
@@ -2,10 +2,70 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace TM2
 {
     class AudioManager
     {
+        List<Song> songs = new List<Song>();
+        List<String> names = new List<String>();
+        float volume;
+        bool increase, fading;
+        float targetVolume;
+        int time, targetTime;
+        public void LoadContent()
+        {
+
+        }
+
+        public void UnloadContent()
+        {
+
+        }
+
+        public void Update()
+        {
+            if (fading)
+            {
+                if (increase && targetVolume != MediaPlayer.Volume && time != targetTime)
+                    MediaPlayer.Volume += .01f;
+                else if (!increase && targetVolume != MediaPlayer.Volume && time != targetTime)
+                    MediaPlayer.Volume -= .01f;
+                else
+                    fading = false;
+            }
+            time++;
+        }
+        //Target time is actually the time between fades representing frames
+        public void fadeIn(float targetVolume, int targetTime)
+        {
+            increase = true;
+            fading = true;
+            this.targetVolume = targetVolume;
+            this.targetTime = targetTime;
+        }
+        //Target time is actually the time between fades representing frames
+        public void fadeOut(float targetVolume, int targetTime)
+        {
+            increase = false;
+            fading = true;
+            this.targetVolume = targetVolume;
+            this.targetTime = targetTime;
+        }
+        public void Play(int songNum)
+        {
+            MediaPlayer.Play(songs[songNum]);
+        }
+        public void Draw()
+        {
+
+        }
     }
 }
